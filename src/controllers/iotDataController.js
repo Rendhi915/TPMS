3dfa3e490d04b60e1fba7598d81f582189bb0da5
Rendhi915 <113291367+Rendhi915@users.noreply.github.com>
2025-11-10 14:06:57 +0700

    fix error update vendor

--- src/controllers/iotDataController.js
+++ src/controllers/iotDataController.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require('../../prisma/generated/client');
-const prisma = new PrismaClient();
+const { prisma } = require('../config/prisma');
 const { broadcastSensorUpdate, broadcastDeviceUpdate } = require('../services/websocketService');
 
 // ==========================================
